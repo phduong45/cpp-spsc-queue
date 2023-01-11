90119f81474880ef6c4508ecb3d08562b5d5b6a1
Phuong Duong <phuongx31@gmail.com>
2023-01-11 14:09:17 +0700

    add closed

--- main.cpp
+++ main.cpp
@@ -8,10 +8,15 @@
 
 class BoundedQueue {
   public:
-    void push(int value) {
+    bool push(int value) {
         {
             std::unique_lock<std::mutex> lock(mutex_);
-            not_full_.wait(lock, [this] { return size_ < data_.size(); });
+            not_full_.wait(lock,
+                           [this] { return size_ < data_.size() || closed_; });
+
+            if (closed_) {
+                return false;
+            }
 
             data_[head_] = value;
             head_ = (head_ + 1) % data_.size();
@@ -19,13 +24,17 @@ class BoundedQueue {
         }
 
         not_empty_.notify_one();
+        return true;
     }
 
-    int pop() {
-        int value{0};
+    bool pop(int& value) {
         {
             std::unique_lock<std::mutex> lock(mutex_);
-            not_empty_.wait(lock, [this] { return size_ > 0; });
+            not_empty_.wait(lock, [this] { return size_ > 0 || closed_; });
+
+            if (size_ == 0 && closed_) {
+                return false;
+            }
 
             value = data_[tail_];
             tail_ = (tail_ + 1) % data_.size();
@@ -33,13 +42,12 @@ class BoundedQueue {
         }
 
         not_full_.notify_one();
-
-        return value;
+        return true;
     }
 
     bool try_push(int value) {
         std::lock_guard<std::mutex> lock(mutex_);
-        if (size_ == data_.size()) {
+        if (closed_ || size_ == data_.size()) {
             return false;
         }
 
@@ -66,6 +74,16 @@ class BoundedQueue {
         return true;
     }
 
+    void close() {
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            closed_ = true;
+        }
+
+        not_empty_.notify_all();
+        not_full_.notify_all();
+    }
+
   private:
     std::array<int, 4> data_{};
     std::size_t head_ = 0;
@@ -74,6 +92,7 @@ class BoundedQueue {
     std::mutex mutex_;
     std::condition_variable not_empty_;
     std::condition_variable not_full_;
+    bool closed_ = false;
 };
 
 int main() {
@@ -82,13 +101,15 @@ int main() {
 
     std::thread producer([&] {
         for (int value = 1; value <= 10; ++value) {
-            queue.push(value);
+            assert(queue.push(value));
         }
     });
 
     std::thread consumer([&] {
+        int value = 0;
         for (int i = 0; i < 10; ++i) {
-            sum += queue.pop();
+            assert(queue.pop(value));
+            sum += value;
         }
     });
 
@@ -113,6 +134,24 @@ int main() {
 
     assert(try_queue.try_push(5));
     std::cout << "try queue api ok\n";
+
+    BoundedQueue close_queue;
+    assert(close_queue.push(1));
+    assert(close_queue.push(2));
+    close_queue.close();
+
+    int closed_value = 0;
+    assert(close_queue.pop(closed_value));
+    assert(closed_value == 1);
+
+    assert(close_queue.pop(closed_value));
+    assert(closed_value == 2);
+
+    assert(!close_queue.pop(closed_value));
+    assert(!close_queue.push(3));
+    assert(!close_queue.try_push(3));
+
+    std::cout << "close queue ok\n";
 
     return 0;
 }
