f78981dd879239a6c6635f4232292ebf9eaeef38
Phuong Duong <phuongx31@gmail.com>
2023-03-31 11:12:37 +0700

    add spsc queue class

---
+++ spsc_queue.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <array>
+#include <cstddef>
+#include <optional>
+#include <utility>
+
+namespace spsc {
+
+template <typename T, std::size_t Capacity>
+class SpscQueue {
+    static_assert(Capacity > 0, "SpscQueue capacity must be greater than 0");
+
+  public:
+    static constexpr std::size_t capacity() {
+        return Capacity;
+    }
+
+    bool try_push(T value) {
+        // TODO
+    }
+
+    std::optional<T> try_pop() {
+        // TODO
+    }
+
+  private:
+    bool is_empty() const {
+        return size_ == 0;
+    }
+
+    bool is_full() const {
+        return size_ == data_.size();
+    }
+
+    std::array<T, Capacity> data_{};
+    std::size_t head_ = 0;
+    std::size_t tail_ = 0;
+    std::size_t size_ = 0;
+};
+}; // namespace spsc
